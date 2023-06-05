afea5020fc1058d937713da64b202b14bf8eeb4e
Devfrank94 <francescomurro94@gmail.com>
2023-06-05 19:31:42 +0200

    complete background center

--- config/menus.php
+++ config/menus.php
@@ -1,49 +1,165 @@
 <?php
 
 return [
-  'header_menu' => [
+'header_menu' => [
     [
-      'href' => "home",
-      'text' => "characters"
+    'href' => "home",
+    'text' => "characters"
     ],
     [
-      'href' => "comics",
-      'text' => "comics"
+    'href' => "comics",
+    'text' => "comics"
     ],
     [
     'href' => "movies",
-      'text' => "movies"
+    'text' => "movies"
     ],
     [
-      'href' => "tv",
-      'text' => "tv"
+    'href' => "tv",
+    'text' => "tv"
     ],
     [
-      'href' => "games",
-      'text' => "games"
+    'href' => "games",
+    'text' => "games"
     ],
     [
-      'href' => "collectibles",
-      'text' => "collectibles"
+    'href' => "collectibles",
+    'text' => "collectibles"
     ],
     [
-      'href' => "videos",
-      'text' => "videos"
+    'href' => "videos",
+    'text' => "videos"
     ],
     [
-      'href' => "fans",
-      'text' => "fans"
+    'href' => "fans",
+    'text' => "fans"
     ],
     [
-      'href' => "news",
-      'text' => "news"
+    'href' => "news",
+    'text' => "news"
     ],
     [
-      'href' => "shop",
-      'text' => "shop"
+    'href' => "shop",
+    'text' => "shop"
     ]
-  ],
+],
 
+'footer_menu' =>   [
+    [
+    'dc Comic' => [
+        [
+        'href' => "#",
+        'text' => "characters"
+        ],
+        [
+        'href' => "#",
+        'text' => "comics"
+        ],
+        [
+        'href' => "#",
+        'text' => "movies"
+        ],
+        [
+        'href' => "#",
+        'text' => "TV"
+        ],
+        [
+        'href' => "#",
+        'text' => "games"
+        ],
+        [
+        'href' => "#",
+        'text' => "videos"
+        ],
+        [
+        'href' => "#",
+        'text' => "news"
+        ]
+    ],
+    'shop' => [
+        [
+        'href' => "#",
+        'text' => "shop DC"
+        ],
+        [
+        'href' => "#",
+        'text' => "shop DC collectibles"
+        ]
+    ]
+    ],
+    [
+    'dc' => [
+        [
+        'href' => "#",
+        'text' => "terms of use"
+        ],
+        [
+        'href' => "#",
+        'text' => "privacy policy (new)"
+        ],
+        [
+        'href' => "#",
+        'text' => "sd choices"
+        ],
+        [
+        'href' => "#",
+        'text' => "advertising"
+        ],
+        [
+        'href' => "#",
+        'text' => "jobs"
+        ],
+        [
+        'href' => "#",
+        'text' => "subscriptions"
+        ],
+        [
+        'href' => "#",
+        'text' => "talent workshops"
+        ],
+        [
+        'href' => "#",
+        'text' => "CPSC certificates"
+        ],
+        [
+        'href' => "#",
+        'text' => "ratings"
+        ],
+        [
+        'href' => "#",
+        'text' => "shop help"
+        ],
+        [
+        'href' => "#",
+        'text' => "contact us"
+        ]
+    ],
+    ],
+    [
+    'sites' => [
+        [
+        'href' => "#",
+        'text' => "DC"
+        ],
+        [
+        'href' => "#",
+        'text' => "MAD magazine"
+        ],
+        [
+        'href' => "#",
+        'text' => "DC kids"
+        ],
+        [
+        'href' => "#",
+        'text' => "DC universe"
+        ],
+        [
+        'href' => "#",
+        'text' => "DC power visa"
+        ],
+        ]
+    ]
+],
 
 
 ];
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,85 @@
-<h1>Footer</h1>
+@php
+  $menu_footer = config('menus.footer_menu');
+
+@endphp
+
+<footer>
+    <div class="background-top">
+      <div class="container">
+        <div class="blue-section d-flex j-cont-bet">
+          <div class="icon-card d-flex al-item-cent j-cont-cent">
+          <img src="/resources/img/buy-comics-digital-comics.png">
+          <span>digital comics</span>
+          </div>
+          <div class="icon-card d-flex al-item-cent j-cont-cent">
+          <img src="../assets/img/buy-comics-merchandise.png">
+          <span>dc merchandise</span>
+          </div>
+          <div class="icon-card d-flex al-item-cent j-cont-cent">
+          <img src="../assets/img/buy-comics-subscriptions.png">
+          <span>subscription</span>
+          </div>
+          <div class="icon-card d-flex al-item-cent j-cont-cent">
+          <img src="../assets/img/buy-comics-shop-locator.png">
+          <span>comic shop locator</span>
+          </div>
+          <div class="icon-card d-flex al-item-cent j-cont-cent">
+          <img src="../assets/img/buy-dc-power-visa.svg">
+          <span>dc power visa</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="background-center">
+        <div class="container d-flex j-cont-bet">
+            <div class="categories d-flex">
+                    @foreach ($menu_footer as $menu_col)
+                        <div class="col">
+                            @foreach ($menu_col as $key => $menu_section )
+                            <h4>{{$key}}</h4>
+                                <nav>
+                                    <ul>
+                                        @foreach ($menu_section as $item)
+                                        <li>
+                                        <a href="">{{$item['text']}}</a>
+                                        </li>
+                                        @endforeach
+                                    </ul>
+                                </nav>
+                            @endforeach
+                        </div>
+                    @endforeach
+                </div>
+                <div class="right">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="background-bottom d-flex">
+      <div class="container d-flex j-cont-bet">
+        <div class="foot-bott-left d-flex al-item-cent j-cont-cent">
+          <span>sign-up now!</span>
+        </div>
+        <div class="foot-bott-right d-flex al-item-cent j-cont-cent">
+          <span>follow us</span>
+          <nav class="d-flex al-item-cent">
+              <ul class="d-flex">
+                {{-- <li v-for="(icon, index) in socialMenu" :key="index">
+                  <a :href="icon.href"><img :src="icon.img"></a>
+                </li> --}}
+              </ul>
+            </nav>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+</footer>
