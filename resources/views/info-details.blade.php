1acd804f615f15d0919cc3fb4622f3f6cd573839
Devfrank94 <francescomurro94@gmail.com>
2023-06-06 19:00:44 +0200

    add style info details

--- resources/views/info-details.blade.php
+++ resources/views/info-details.blade.php
@@ -7,15 +7,24 @@
 @section('content')
 
 {{-- @dd($card); --}}
+@dump($card);
 <main>
     <div class="sec-blue d-flex j-cont-cent al-item-cent">
         <h2>Info Details</h2>
     </div>
 
 
-    <div class="card al-item-cent">
+    <div class="card d-flex j-cont-cent al-item-cent flex-dir-col">
         <img src="{{$card['thumb']}}" alt="">
-        <span>{{$card['title']}}</span>
+        <div class="info-card">
+            <h3>Titolo: {{$card['title']}}</h3>
+            <h4>Prezzo: {{$card['price']}}</h4>
+            <h4>Serie di appartenenza: {{$card['series']}}</h4>
+            <h4>Uscita: {{$card['sale_date']}}</h4>
+            <h4>Tipo: {{$card['type']}}</h4>
+            <p>Descrizione: {{$card['description']}}</p>
+
+        </div>
     </div>
 
 </main>
